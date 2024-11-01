01844c78ca8114aa7886e1139decbb6d76a3bb73
Shauk3 <jaofelipes22@gmail.com>
2024-11-01 17:11:01 -0400

    Projeto V0.5

--- Projeto/java.js
+++ Projeto/java.js
@@ -1,9 +1,42 @@
-const data = document.getElementById("data")
-const horario = document.getElementById("hora")
+const imgs = window.document.getElementById('Containe_img')
+const img =  window.document.querySelectorAll("#Containe_img img")
 
-const datareal = new Date()
-let dia = datareal.getDate()
-let mes = datareal.getDay()
-const datar = dia+""+mes+""+ datareal.getFullYear()
+let idx = 0
 
-data.innerHTML = datareal
+function carrosel(){
+    idx++
+
+    if(idx > img.length - 1){
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 700}px)`
+}
+
+setInterval(carrosel, 1800)
+
+
+var datafutura = new Date("November 13, 2024 20:00").getTime();
+var dias,horas,minutos,segundos;
+
+setInterval(function(){
+
+    var dataatual = new Date().getTime();
+    var segundosTotal = (datafutura - dataatual) / 1000;
+
+    dias = parseInt(segundosTotal/86400);
+    segundosTotal = segundosTotal%86400;
+
+    horas = parseInt(segundosTotal/3600);
+    segundosTotal = segundosTotal%3600;
+
+    minutos = parseInt(segundosTotal/60);
+    segundos = parseInt(segundosTotal%60);
+
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+},1000)
+
